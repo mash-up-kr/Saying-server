0501732d1e2f2d103f664c284a51f6f3a7b86773
KIM-JUNG-MIN <iridescent.kjm17@gmail.com>
2017-07-30 00:07:52 +0900

    feature: 계정명 중복 체크

--- Saying/Account/urls.py
+++ Saying/Account/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import register_user, update_profile, activate
+from .views import register_user, update_profile, activate, acc_check
 
 
 urlpatterns = [
+    url(r'^check$', acc_check, name='check'),
     url(r'^register$', register_user, name='register'),
     url(r'^activate/(?P<userid>[^/]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         activate, name='activate'),
--- Saying/Account/views.py
+++ Saying/Account/views.py
@@ -1,5 +1,4 @@
 from django.http import HttpResponse, HttpResponseBadRequest
-from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import get_user_model
 from .models import UserProfile
@@ -12,46 +11,60 @@ User = get_user_model()
 
 
 @csrf_exempt
-@require_POST
+def acc_check(request):
+    if request.method == "POST":
+        user_acc = request.POST.get('acc')
+
+        if User.objects.filter(user_acc=user_acc).exists():
+            return HttpResponse("already exists user")
+        else:
+            return HttpResponse("OK")
+    return HttpResponseBadRequest("required POST method")
+
+
+@csrf_exempt
 def register_user(request):
-    # get data from post
-    user_acc = request.POST.get('acc')
-    password = request.POST.get('pwd')
-    nickname = request.POST.get('nm')
-    age = request.POST.get('age')
-    gender = request.POST.get('gender')
+    if request.method == "POST":
+        user_acc = request.POST.get('acc')
+        password = request.POST.get('pwd')
+        nickname = request.POST.get('nm')
+        age = request.POST.get('age')
+        gender = request.POST.get('gender')
 
-    if not all([user_acc, password, nickname, age, gender]):
-        return HttpResponseBadRequest("No key or null value in POST params")
-    else:
-        user = User.objects.create_user(user_acc=user_acc, password=password)
-        user.userprofile.nickname = nickname
-        user.userprofile.age = age
-        user.userprofile.gender = gender
-        user.save()
+        if not all([user_acc, password, nickname, age, gender]):
+            return HttpResponseBadRequest("No key or null value in POST params")
+        else:
+            user = User.objects.create_user(user_acc=user_acc, password=password)
+            user.userprofile.nickname = nickname
+            user.userprofile.age = age
+            user.userprofile.gender = gender
+            user.save()
 
-        subject = 'Activate Your Saying Account'
-        message = render_to_string('account_activation_email.html', {
-            'user': user.userprofile.nickname,
-            'domain': 'saying@mashup-dev.org',
-            'uid': str(user.pk),
-            'token': account_activation_token.make_token(user),
-        })
+            subject = 'Activate Your Saying Account'
+            message = render_to_string('account_activation_email.html', {
+                'user': user.userprofile.nickname,
+                'domain': 'saying@mashup-dev.org',
+                'uid': str(user.pk),
+                'token': account_activation_token.make_token(user),
+            })
 
-        send_mail(subject, message, 'saying@mashup-dev.org', [user_acc])
+            send_mail(subject, message, 'saying@mashup-dev.org', [user_acc])
 
-        return HttpResponse("success")
+            return HttpResponse("register success")
+    return HttpResponseBadRequest("required POST method")
 
 
 @csrf_exempt
 def update_profile(request):
     if request.method == "POST":
-        user = UserProfile.objects.get(userid=request.POST['userid'])
-        user.nickname = request.POST['nickname']
-        user.save()
+        upload = request.FILES['file']
+        # user = UserProfile.objects.get(userid=request.POST['userid'])
+        # user.nickname = request.POST['nickname']
+        # user.save()
         # data = serializers.serialize("json", User.objects.all())
         # return HttpResponse(data, content_type='application/json')
-        return HttpResponse("success")
+        print(upload)
+        return HttpResponse("receive success")
     return HttpResponse("no data")
 
 
